d218279ccdafad16b04a46fa905ba440ee45a529
Lewis Bray <32199493+LewisBray@users.noreply.github.com>
2024-08-07 11:39:51 +0100

    Changed build file to work on new laptop

--- build.bat
+++ build.bat
@@ -1,13 +1,14 @@
 @ECHO OFF
 
-SET "msvc_path=C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30133"
+SET "msvc_path=C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.38.33130"
 SET "sdk_path=C:\Program Files (x86)\Windows Kits\10"
 SET vc_tools_path="%msvc_path%\bin\Hostx64\x64"
-SET system_libs_path="%sdk_path%\Lib\10.0.19041.0\um\x64"
-SET rc_tool_path="%sdk_path%\bin\10.0.19041.0\x64"
+SET system_libs_path="%sdk_path%\Lib\10.0.22621.0\um\x64"
+SET rc_tool_path="%sdk_path%\bin\10.0.22621.0\x64"
 
 SET "common_compiler_flags=-c -g"
-SET "clang_cpp_compiler_flags=-std=c++1z -Weverything -Wno-c++98-compat -Wno-c++98-compat-pedantic"
+REM SET "clang_cpp_compiler_flags=-std=c++1z -Weverything -Wno-c++98-compat -Wno-c++98-compat-pedantic"
+SET "clang_cpp_compiler_flags=-std=c++1z"
 SET "common_linker_flags=/NODEFAULTLIB /DEBUG /SUBSYSTEM:WINDOWS /INCREMENTAL:NO"
 
 clang src\tetris_ai.cpp %common_compiler_flags% %clang_cpp_compiler_flags%
